cf9a3374787380e6d6f39c1cbc6afc584bdc2a0b
Trey Griffith <trey.griffith@gmail.com>
2014-04-28 14:41:23 -0700

    add tests

---
+++ test/oz-text.js
@@ -0,0 +1,36 @@
+
+var Oz = require('oz');
+var assert = require('assert');
+var text = require('text');
+var children = require('children');
+
+describe('Rendering', function(){
+  it('should set text values', function(){
+    var el = children(Oz.render('<div><p oz-text="name"></p></div>', { name: 'Tobi' }))[0];
+    assert('Tobi' == text(children(el)[0]));
+  });
+
+  it('should not display undefined as a text node', function(){
+    var els = children(Oz.render('<div oz-text="name"></div>', {}));
+
+    assert(text(els[0]) === '');
+  });
+
+});
+
+describe("Updating", function() {
+
+
+  it('should update text values', function(){
+
+    var template = Oz('<div><p oz-text="name"></p></div>');
+    var el = children(template.render({ name: 'Tobi' }))[0];
+
+    assert('Tobi' == text(children(el)[0]));
+
+    template.update({ name: 'Brian' });
+
+    assert('Brian' == text(children(el)[0]));
+  });
+
+});
